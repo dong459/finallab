8133cd1631d494935c125b8d7377db1349eebb4c
dong459 <khwanzone123@gmail.com>
2022-03-08 16:00:29 +0700

    first commit

---
+++ frontend/src/components/name.js
@@ -0,0 +1,11 @@
+function MyName() {
+    return (
+    <div className="MyName">
+    <div>นายขวัญชัย วุฒิวงศ์
+    </div>
+    <div>610112418004</div>
+    </div>
+
+    );
+}
+export default MyName;
